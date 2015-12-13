9dd29cc976911e4e1c860d63fc08a0c7b2a52adf
Thomas B Homburg <thomas@homburg.dk>
2015-12-13 21:14:48 +0100

    Use q92 for jpegs

--- backend/app/services/ImageProxy.scala
+++ backend/app/services/ImageProxy.scala
@@ -4,5 +4,6 @@ import play.api.Play.current
 
 object ImageProxy {
   private lazy val baseUrl = current.configuration.getString("imageProxy.baseUrl").get.stripSuffix("/")
-  def width(width: Int, url: String) = s"$baseUrl/$width/$url"
+  private val quality = 92
+  def width(width: Int, url: String) = s"$baseUrl/${width}x,q${quality}/$url"
 }
